7153efaf1d11188534989ad0981ca9f430209f82
hungdao1310 <93567684+hungdao1310@users.noreply.github.com>
2023-09-30 08:50:10 +0700

    Add files via upload

---
+++ ber_cal.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void split_text_file(const char* file_path, const char* split_character, const char* output_file_path) {
+    FILE* input_file = fopen(file_path, "r");
+    if (input_file == NULL) {
+        printf("Unable to open the input file.\n");
+        return;
+    }
+
+    // Get the size of the input file
+    fseek(input_file, 0, SEEK_END);
+    long input_file_size = ftell(input_file);
+    rewind(input_file);
+
+    // Allocate memory to store the input file contents
+    char* text = (char*)malloc((input_file_size + 1) * sizeof(char));
+    if (text == NULL) {
+        printf("Memory allocation failed.\n");
+        fclose(input_file);
+        return;
+    }
+
+    // Read the input file contents
+    size_t read_size = fread(text, sizeof(char), input_file_size, input_file);
+    if (read_size != input_file_size) {
+        printf("Error reading the input file.\n");
+        free(text);
+        fclose(input_file);
+        return;
+    }
+
+    text[input_file_size] = '\0';  // Add null terminator
+
+    // Find the split character in the text
+    char* split_index = strstr(text, split_character);
+    if (split_index != NULL) {
+        // Calculate the length of the right-half portion
+        size_t right_half_length = strlen(split_index + strlen(split_character));
+
+        // Open the output file for writing
+        FILE* output_file = fopen(output_file_path, "w");
+        if (output_file == NULL) {
+            printf("Unable to open the output file.\n");
+            free(text);
+            fclose(input_file);
+            return;
+        }
+
+        // Write the right-half portion to the output file
+        fwrite(split_index + strlen(split_character), sizeof(char), right_half_length, output_file);
+
+        fclose(output_file);
+        printf("Successfully written the right-half portion to %s\n", output_file_path);
+    } else {
+        printf("The split character '%s' was not found in the file.\n", split_character);
+    }
+
+    free(text);
+    fclose(input_file);
+}
+
+float ber_cal(const char* file1_path, const char* file2_path) {
+    FILE* file1 = fopen(file1_path, "r");
+    FILE* file2 = fopen(file2_path, "r");
+
+    if (file1 == NULL || file2 == NULL) {
+        printf("Unable to open input files.\n");
+        return 0;
+    }
+
+    unsigned int xor_sum = 0;
+    int byte1, byte2;
+    int bitCount = 0;
+    int character;
+    while ((byte1 = fgetc(file1)) != EOF && (byte2 = fgetc(file2)) != EOF) {
+        // Count the number of bits in each character
+        unsigned char byte = (unsigned char)byte1;
+        for (int i = 0; i < 8; i++) {
+            if (byte & (1 << i)) {
+                bitCount++;
+            }
+        }
+        xor_sum ^= byte1 ^ byte2;
+    }
+    printf("Bit count: %u\n",bitCount);
+
+    fclose(file1);
+    fclose(file2);
+    
+    return (float)xor_sum/bitCount;;
+}
+
+int main() {
+    //createRandomFile();
+    //split_text_file("teratermabcd.log", "seperateforme123", "output1.txt");
+    float result = ber_cal("output123.txt", "output1.txt");
+    printf("BER results: %.2f\n", result);
+    return 0;
+}
